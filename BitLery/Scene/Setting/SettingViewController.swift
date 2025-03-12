39c4aa102663cb3c0a04a09b8a2409ac97ec139e
withseon <dlstjsvv@gmail.com>
2025-03-12 23:32:26 +0900

    Add: 설정 화면 추가

--- BitLery/Scene/Setting/SettingViewController.swift
+++ BitLery/Scene/Setting/SettingViewController.swift
@@ -6,10 +6,33 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import SnapKit
 
 final class SettingViewController: BaseViewController {
+    private let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
     }
+    
+    override func configureNavigation() {
+        super.configureNavigation()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(navigationBar)
+    }
+    
+    override func configureLayout() {
+        navigationBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(44)
+        }
+    }
+    
+    override func configureView() {
+        
+    }
+
 }
